4c9e63452c16c7b3502fbdd469e81e01a143006c
Smertig <akaraevz@mail.ru>
2021-09-18 15:20:24 +0300

    Partially support ARM64 (user should provide rcmp::make_raw_hook implementation)

--- source/detail/arch/arm64/impl.cpp
+++ source/detail/arch/arm64/impl.cpp
@@ -1,14 +1,13 @@
 #include <rcmp/memory.hpp>
-#include <rcmp/codegen.hpp>
 
-// returns relocated original address
-rcmp::address_t rcmp::make_raw_hook(rcmp::address_t original_function, rcmp::address_t wrapper_function) {
-    static_cast<void>(original_function);
-    static_cast<void>(wrapper_function);
-    throw rcmp::error("rcmp::make_raw_hook is not implemented yet");
-}
+/*
+ * No implementation, user should provide its own
+ *
+ * // returns relocated original address
+ * rcmp::address_t rcmp::make_raw_hook(rcmp::address_t original_function, rcmp::address_t wrapper_function);
+*/
 
 std::size_t rcmp::opcode_length(rcmp::address_t address) {
     static_cast<void>(address);
-    throw rcmp::error("rcmp::opcode_length is not implemented yet");
+    return 4;
 }
