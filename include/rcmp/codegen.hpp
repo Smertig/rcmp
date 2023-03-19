d24601334683d32790cbbf2a3fbd1b75bc8d02f7
Smertig <akaraevz@mail.ru>
2023-03-19 14:47:02 +0200

    Introduce `hook_state_t`

--- include/rcmp/codegen.hpp
+++ include/rcmp/codegen.hpp
@@ -14,36 +14,45 @@ namespace rcmp {
 
 namespace detail {
 
+template <class GenericSignature, class Hook>
+struct hook_state_t {
+    using original_sig_t = rcmp::from_generic_signature<GenericSignature>;
+    using hook_t         = Hook;
+
+    std::optional<hook_t> hook     = std::nullopt;
+    original_sig_t        original = nullptr;
+};
+
 template <class GenericSignature, class Hook, class... Tags>
-struct hook_impl;
+class hook_impl;
 
 template <class Ret, class... Args, cconv Convention, class Hook, class... Tags>
-struct hook_impl<generic_signature_t<Ret(Args...), Convention>, Hook, Tags...> {
+class hook_impl<generic_signature_t<Ret(Args...), Convention>, Hook, Tags...> {
     using generic_sig_t  = generic_signature_t<Ret(Args...), Convention>;
     using original_sig_t = rcmp::from_generic_signature<generic_sig_t>;
     using hook_t         = Hook;
 
     static_assert(std::is_invocable_r_v<Ret, hook_t, original_sig_t, Args...>);
 
-    inline static std::optional<hook_t> m_hook     = std::nullopt;
-    inline static original_sig_t        m_original = nullptr;
+    inline static hook_state_t<generic_sig_t, hook_t> m_state;
 
     static Ret call_hook_impl(Args... args) {
-        assert(m_hook != std::nullopt);
-        assert(m_original != nullptr);
-        return (*m_hook)(m_original, std::forward<Args>(args)...);
+        assert(m_state.hook != std::nullopt);
+        assert(m_state.original != nullptr);
+        return (*m_state.hook)(m_state.original, std::forward<Args>(args)...);
     }
 
     static inline constexpr original_sig_t call_hook = with_signature<call_hook_impl, generic_sig_t>;
 
+public:
     template <class Policy>
     static void do_hook(rcmp::address_t address, hook_t hook) {
-        if (m_original != nullptr) {
+        if (m_state.original != nullptr) {
             throw rcmp::error("double hook of %" PRIXPTR, address.as_number());
         }
 
-        m_hook.emplace(std::move(hook));
-        m_original = Policy::make_raw_hook(address, rcmp::bit_cast<void*>(call_hook)).template as<original_sig_t>();
+        m_state.hook.emplace(std::move(hook));
+        m_state.original = Policy::make_raw_hook(address, rcmp::bit_cast<void*>(call_hook)).template as<original_sig_t>();
     }
 };
 
